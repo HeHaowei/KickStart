8a96af73e44214601e0a9b39abc6047da86da749
HeHaowei <35795685+HeHaowei@users.noreply.github.com>
2020-08-19 14:37:13 +0800

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int T;
+    for (int t = 0; t < T; t++) {
+        
+        
+        
+        cout << "Case #" << t + 1 << ": " <<  << endl;
+    }
+    
+}
